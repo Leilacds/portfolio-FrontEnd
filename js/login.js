b7ebf10abf523205b81800b780f69c47d4b1cd8d
Leilacds <leila1995@live.com.ar>
2023-01-15 17:48:11 -0300

    Pantalla de login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,21 @@
- /* $('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });  */
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+
+    console.log(data)
+
+if(data.username=="leila" && data.password=="leila") {
+    console.log("Ha ingresado");
+    window.location.href="index.html";
+}
+else {
+    alert("El usuario o la contraseña es incorrecto");
+}
+})
